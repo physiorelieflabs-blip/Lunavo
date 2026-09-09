03dc49f231312bde2f5d43546a48351dcafa97d6
physiorelieflabs-blip <physiorelieflabs@gmail.com>
2026-09-09 11:29:58 +0100

    feat(ui): centralize Lunavo application exports

---
+++ artifacts/ts-commerce/src/lunavo-bootstrap.ts
@@ -0,0 +1,7 @@
+import "./styles/lunavo-app.css";
+export { default as LunavoCommandCenter } from "./pages/lunavo-command-center";
+export { default as LunavoPayments } from "./pages/lunavo-payments";
+export { default as LunavoCommerceSuite } from "./pages/lunavo-commerce-suite";
+export { default as LunavoAdmin } from "./pages/lunavo-admin";
+export { default as LunavoReferrals } from "./pages/lunavo-referrals";
+export { default as LunavoSystemSettings } from "./pages/lunavo-system-settings";
